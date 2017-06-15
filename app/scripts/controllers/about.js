49c1eb36ed7cc349e249790485d99457ee239379
Rob Thijssen <rthijssen@gmail.com>
2017-06-15 18:03:47 +0300

    minion manager

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name minionsManagedNgApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the minionsManagedNgApp
+ */
+angular.module('minionsManagedNgApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name minionsManagedNgApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the minionsManagedNgApp
+ */
+angular.module('minionsManagedNgApp')
+  .controller('MainCtrl', function ($scope, mmApi) {
+    $scope.workerTypes = ['gecko-t-win7-32', 'gecko-t-win7-32-gpu', 'gecko-t-win10-64', 'gecko-t-win10-64-gpu', 'gecko-1-b-win2012', 'gecko-2-b-win2012', 'gecko-3-b-win2012'];
+    $scope.dataCenters = ['use1', 'use2', 'usw1', 'usw2', 'euc1'];
+    mmApi.query({}, function (minions) {
+      $scope.counts = {};
+      $scope.workerTypes.forEach(function(workerType) {
+        $scope.counts[workerType] = { count: 0 };
+        $scope.dataCenters.forEach(function(dataCenter) {
+          $scope.counts[workerType][dataCenter] = minions.filter(function(minion){ return minion.workerType == workerType && minion.dataCenter == dataCenter; }).length;
+          $scope.counts[workerType].count += $scope.counts[workerType][dataCenter];
+        });
+      });
+      $scope.minions = minions;
+    });
+  });
