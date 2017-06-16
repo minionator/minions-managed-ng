556cb62885e4b209add5197643fe7215423aaa4e
Rob Thijssen <rthijssen@gmail.com>
2017-06-16 16:35:30 +0300

    tabbed workerTypes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,11 @@ angular.module('minionsManagedNgApp')
   .controller('MainCtrl', function ($scope, mmApi) {
     $scope.workerTypes = ['gecko-t-win7-32', 'gecko-t-win7-32-gpu', 'gecko-t-win10-64', 'gecko-t-win10-64-gpu', 'gecko-1-b-win2012', 'gecko-2-b-win2012', 'gecko-3-b-win2012'];
     $scope.dataCenters = ['use1', 'use2', 'usw1', 'usw2', 'euc1'];
+    $scope.selected = {
+      workerType: $scope.workerTypes[0],
+      dataCenter: $scope.dataCenters[0]
+    };
+    $scope.loading = true;
     mmApi.query({}, function (minions) {
       $scope.counts = {};
       $scope.workerTypes.forEach(function(workerType) {
@@ -20,6 +25,13 @@ angular.module('minionsManagedNgApp')
           $scope.counts[workerType].count += $scope.counts[workerType][dataCenter];
         });
       });
-      $scope.minions = minions;
+      $scope.allMinions = minions;
+      $scope.minions = minions.filter(function(minion){ return minion.workerType == $scope.selected.workerType; });
+      $scope.loading = false;
     });
+    $scope.getData = function(){
+      $scope.loading = true;
+      $scope.minions = $scope.allMinions.filter(function(minion){ return minion.workerType == $scope.selected.workerType; });
+      $scope.loading = false;
+    }
   });
