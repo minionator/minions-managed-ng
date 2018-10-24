9fff2bb7a54194674f90e78a0e35835fe4534265
Rob Thijssen <rthijssen@gmail.com>
2018-10-24 17:28:42 +0300

    font awesome!

--- Gruntfile.js
+++ Gruntfile.js
@@ -426,6 +426,16 @@ module.exports = function (grunt) {
           cwd: '.',
           src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: '.',
+          src: 'bower_components/components-font-awesome/webfonts/*',
+          dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: '.',
+          src: 'bower_components/components-font-awesome/scss/*',
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
